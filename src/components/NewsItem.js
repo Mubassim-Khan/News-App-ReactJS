580e64fea4faca36e6b6dcad7eea18674d968abb
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2023-07-10 21:12:23 +0500

    #25 Completed - Added Constructor which will control the state of articles. - Added props of Image URL & news URL. - 'Articles' array would have all the response of API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {title, description} = this.props;
+    let { title, description, imgURL, newsURL } = this.props;
     return (
-    <div className="card" style={{ width: "18rem"}}>
-      <img src="https://talksport.com/wp-content/uploads/sites/5/2023/07/PM-TALKSPORT-Blog-8th-July-AM.jpg?strip=all&quality=100&w=1500&h=1000&crop=1" className="card-img-top" alt="..."/>
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
-        <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
+      <div className="card my-3" style={{ width: "18rem" }}>
+        <img src={imgURL} className="card-img-top" alt="image" />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/newsdetails" className="btn btn-sm btn-primary">Read More</a>
+        </div>
       </div>
-    </div>
     )
   }
 }
