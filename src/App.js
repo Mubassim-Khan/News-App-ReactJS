6bb1a468c744cae5c1915fa918f4a609c2e5fdb1
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2023-07-18 19:17:54 +0500

    #36 Completed - Added Custom Loading bar at the top - Made some other changes on source badge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import NavBar from './components/NavBar'
 import NewsSection from './components/NewsSection'
+import LoadingBar from 'react-top-loading-bar'
 import {
   BrowserRouter,
   Routes,
@@ -8,6 +9,14 @@ import {
 } from "react-router-dom"
 
 export default class App extends Component {
+
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress) => {
+    this.setState({ progress: progress })
+  }
 
   pageSize = 9;
   country = "us";
@@ -17,15 +26,20 @@ export default class App extends Component {
       <div>
         <BrowserRouter>
           <NavBar />
+          <LoadingBar
+            color='#f11946'
+            progress={this.state.progress}
+            height={3}
+          />
           <Routes>
-            <Route exact path='/' element={<NewsSection key="general" pageSize={this.pageSize} country={this.country} category="general" />}></Route>
-            <Route exact path='/business' element={<NewsSection key="business" pageSize={this.pageSize} country={this.country} category="business" />}></Route>
-            <Route exact path='/entertainment' element={<NewsSection key="entertainment" pageSize={this.pageSize} country={this.country} category="entertainment" />}></Route>
-            <Route exact path='/general' element={<NewsSection key="general" pageSize={this.pageSize} country={this.country} category="general" />}></Route>
-            <Route exact path='/health' element={<NewsSection key="health" pageSize={this.pageSize} country={this.country} category="health" />}></Route>
-            <Route exact path='/science' element={<NewsSection key="science" pageSize={this.pageSize} country={this.country} category="science" />}></Route>
-            <Route exact path='/sports' element={<NewsSection key="sports" pageSize={this.pageSize} country={this.country} category="sports" />}></Route>
-            <Route exact path='/technology' element={<NewsSection key="technology" pageSize={this.pageSize} country={this.country} category="technology" />}></Route>
+            <Route exact path='/' element={<NewsSection key="general" setProgress={this.setProgress} pageSize={this.pageSize} country={this.country} category="general" />}></Route>
+            <Route exact path='/business' element={<NewsSection key="business" setProgress={this.setProgress} pageSize={this.pageSize} country={this.country} category="business" />}></Route>
+            <Route exact path='/entertainment' element={<NewsSection key="entertainment" setProgress={this.setProgress} pageSize={this.pageSize} country={this.country} category="entertainment" />}></Route>
+            <Route exact path='/general' element={<NewsSection key="general" setProgress={this.setProgress} pageSize={this.pageSize} country={this.country} category="general" />}></Route>
+            <Route exact path='/health' element={<NewsSection key="health" setProgress={this.setProgress} pageSize={this.pageSize} country={this.country} category="health" />}></Route>
+            <Route exact path='/science' element={<NewsSection key="science" setProgress={this.setProgress} pageSize={this.pageSize} country={this.country} category="science" />}></Route>
+            <Route exact path='/sports' element={<NewsSection key="sports" setProgress={this.setProgress} pageSize={this.pageSize} country={this.country} category="sports" />}></Route>
+            <Route exact path='/technology' element={<NewsSection key="technology" setProgress={this.setProgress} pageSize={this.pageSize} country={this.country} category="technology" />}></Route>
           </Routes>
         </BrowserRouter>
       </div>
--- src/components/NewsSection.js
+++ src/components/NewsSection.js
@@ -33,15 +33,19 @@ export default class NewsSection extends Component {
   }
 
   async updateNewsSection() {
+    this.props.setProgress(10);
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=66d2128a876d4de595fac26a02f73a16&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(50);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false
     });
+    this.props.setProgress(100);
     console.log(parsedData)
   }
 
@@ -60,7 +64,7 @@ export default class NewsSection extends Component {
   }
 
   fetchData = async () => {
-    this.setState({page: this.state.page + 1 })
+    this.setState({ page: this.state.page + 1 })
     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=66d2128a876d4de595fac26a02f73a16&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -74,7 +78,7 @@ export default class NewsSection extends Component {
     return (
       <>
         <h1 className="text-center" style={{ margin: "30px 0px" }} >MAK News - Top {this.firstLetterCap(this.props.category)} Headlines</h1>
-        {this.state.loading && <Spinner/>}
+        {this.state.loading && <Spinner />}
         <InfiniteScroll
           dataLength={this.state.articles.length}
           next={this.fetchData}
