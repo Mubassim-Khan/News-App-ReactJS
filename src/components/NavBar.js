27e0474976289c83f373ac08c30be3dc95f40219
Mubassim Ahmed Khan <mubassimkhan@gmail.com>
2023-07-19 21:23:01 +0500

    Modified Application

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,10 @@ const NavBar = () => {
     return (
         <nav className="navbar fixed-top navbar-expand-lg bg-dark navbar-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">MAK News</Link>
+            <Link to="/">
+            <img src="https://pbs.twimg.com/profile_images/1220285531164233729/A98RISKc_400x400.jpg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top" style={{borderRadius: "100%", height: "50px", width: "50px"}} />
+            </Link>    
+                <Link className="navbar-brand mx-3" to="/">MAK News</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
